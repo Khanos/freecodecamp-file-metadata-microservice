f32fc8c1ecdca34ab8a97ba4152f4fc8f035e0a2
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2019-10-29 17:21:40 +0100

    done

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer  = require('multer');
+var upload = multer({ dest: '/public/uploads/' })
 
 var app = express();
 
@@ -17,6 +19,23 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  if(req.file){
+    if(req.file.size < 10000000){
+      return res.json({
+        "name": req.file.originalname,
+        "type": req.file.mimetype,
+        "size": req.file.size
+      });
+    } else {
+      return res.send('Wow, that\'s a big file, please use less than 10mb');  
+    }
+    
+  } else {
+    return res.send('No file uploaded');
+  }
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
